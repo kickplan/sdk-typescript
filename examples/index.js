cf073fd46d686aa9e1d391fec47b1c928b665c04
willgu29 <willgu29@gmail.com>
2024-01-11 17:27:43 -0500

    update examples

--- examples/index.js
+++ examples/index.js
@@ -2,13 +2,27 @@ const { KickplanApi } = require('sdk-typescript')
 
 async function exampleTests() {
 	const client = new KickplanApi({
-		// apiKey: 'e8852581-a84c-44ae-a876-86bdf796cd50',
-		baseUrl: 'https://api.kickplan.io'
+		apiKey: 'e8852581-a84c-44ae-a876-86bdf796cd50',
+		baseUrl: ' https://demo-control.proxy.kickplan.io'
 	})
 
-	await client.features.resolve()
-	await client.features.resolveAccount('123')
-	await client.features.isFeatureAvailableForAccount('test', '123')
+	/*      FEATURES      */
+	// await client.features.resolve()
+	const result = await client.features.resolveWithAccount('123')
+	// console.log(result)
+	const result2 = await client.features.isFeatureAvailableForAccount('test', '123')
+	// console.log(result2)
+
+	/*      ACCOUNTS       */
+	
+		// const result3 = await client.accounts.post({ key: 'features', plans: ['123'] })
+		// console.log(result3)
+
+
+
+	// /*     METRICS        */
+	const result4 = await client.metrics.setMetricsKey({ key: 'features', value: true, timestamp: new Date() })
+	console.log(result4)
 }
 
 exampleTests()
