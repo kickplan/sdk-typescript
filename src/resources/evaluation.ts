9d100d4e8c25ec00d4d07c3bd5cc0e80bad5c77d
willgu29 <willgu29@gmail.com>
2024-01-11 23:10:54 -0500

    naive implementation of evaluation

--- src/resources/evaluation.ts
+++ src/resources/evaluation.ts
@@ -6,3 +6,31 @@ export type JsonValue = PrimitiveValue | JsonObject | JsonArray;
 
 
 export type FlagValue = boolean | string | number | JsonValue;
+
+export class Evaluation {
+	getBooleanValue(key: string, value: FlagValue): boolean {
+		if (typeof value === 'boolean') {
+			return value
+		}
+		throw Error('Value is not a boolean')
+	}
+	getStringValue(key: string, value: FlagValue): string {
+		if (typeof value === 'string') {
+			return value
+		}
+		throw Error('Value is not a string')
+	}
+	getNumberValue(key: string, value: FlagValue): number {
+		if (typeof value === 'number') {
+			return value
+		}
+		throw Error('Value is not a number')
+	}
+	getObjectValue(key: string, value: FlagValue): JsonObject {
+		if (typeof value === 'object') {
+			return value
+		}
+		throw Error('Value is not an object')
+	}
+
+}
