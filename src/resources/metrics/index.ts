0bdb6bf3d713dc51273f3b00e3fe27a9c31cd826
M. Nicolás Quintana Villalobos <99192045+mnicoq@users.noreply.github.com>
2024-06-14 16:51:50 -0300

    Add missing params to Metrics SDK

--- src/resources/metrics/index.ts
+++ src/resources/metrics/index.ts
@@ -3,12 +3,14 @@ import { MetricsRequest, MetricsResponse } from './types';
 
 export class Metrics extends Base {
 	setMetricsKey(data: MetricsRequest): Promise<MetricsResponse> {
-		return this.request(`/api/metrics/${data.key}/set`, {
-			method: 'POST',
-			body: JSON.stringify({ value: data.value, timestamp: data.timestamp })
-		})	
+    return this.request(`/api/metrics/${data.key}/set`, {
+      method: 'POST',
+      body: JSON.stringify({
+          value: data.value,
+          account_key: data.account_key,
+          time: data.time,
+          idempotency_key: data.idempotency_key
+      })
+    });
 	}
 }
-
-
-
--- src/resources/metrics/types.ts
+++ src/resources/metrics/types.ts
@@ -1,11 +1,13 @@
 import { FlagValue } from '../evaluation';
 
 export declare type MetricsRequest = {
-  key: string;
-  value?: FlagValue;
-  timestamp: Date;
-}
+    key: string;
+    account_key: string;
+    value: FlagValue;
+    time?: Date;
+    idempotency_key?: string;
+};
 
 export declare type MetricsResponse = {
-  metrics: any;
-}
+    metrics: any;
+};
