ceb90f2a39a844852f89d985fe2fb784e7034397
M. Nicolás Quintana Villalobos <99192045+mnicoq@users.noreply.github.com>
2024-06-25 12:08:12 -0300

    Fix indentation

--- src/resources/metrics/index.ts
+++ src/resources/metrics/index.ts
@@ -2,16 +2,16 @@ import { Base } from '../base';
 import { MetricsRequest, MetricsResponse } from './types';
 
 export class Metrics extends Base {
-	setMetricsKey(data: MetricsRequest): Promise<MetricsResponse> {
+  setMetricsKey(data: MetricsRequest): Promise<MetricsResponse> {
     return this.request(`/api/metrics/set`, {
       method: 'POST',
       body: JSON.stringify({
-          key: data.key,
-          value: data.value,
-          account_key: data.account_key,
-          time: data.time,
-          idempotency_key: data.idempotency_key
+        key: data.key,
+        value: data.value,
+        account_key: data.account_key,
+        time: data.time,
+        idempotency_key: data.idempotency_key
       })
     });
-	}
+  }
 }
--- src/resources/metrics/types.ts
+++ src/resources/metrics/types.ts
@@ -1,13 +1,13 @@
 import { FlagValue } from '../evaluation';
 
 export declare type MetricsRequest = {
-    key: string;
-    account_key: string;
-    value: FlagValue;
-    time?: Date;
-    idempotency_key?: string;
+  key: string;
+  account_key: string;
+  value: FlagValue;
+  time?: Date;
+  idempotency_key?: string;
 };
 
 export declare type MetricsResponse = {
-    metrics: any;
+  metrics: any;
 };
