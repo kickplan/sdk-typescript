1c80acb9004e003f9ba94eaf783ea5855529c185
M. Nicolás Quintana <99192045+mnicoq@users.noreply.github.com>
2024-06-26 10:21:57 -0300

    Update Metrics SDK to match Ruby's (#7)
    * Add missing params to Metrics SDK
    
    * Update Metrics endpoint
    
    * Fix indentation

--- examples/index.js
+++ examples/index.js
@@ -1,33 +1,30 @@
 const { KickplanApi } = require('sdk-typescript')
 
 async function exampleTests() {
-	const client = new KickplanApi({
-		apiKey: process.env.KICKPLAN_API_KEY,
-		baseUrl: ' https://demo-control.proxy.kickplan.io'
-	})
+  const client = new KickplanApi({
+    apiKey: process.env.KICKPLAN_API_KEY,
+    baseUrl: ' https://demo-control.proxy.kickplan.io'
+  })
 
-	/*      FEATURES      */
-	// await client.features.resolve()
-	const result = await client.features.resolveWithAccount('123')
-	console.log(result)
-	const result2 = await client.features.isFeatureAvailableForAccount('test', '123')
-	console.log(result2)
+  /*      FEATURES      */
+  // await client.features.resolve()
+  const result = await client.features.resolveWithAccount('123')
+  console.log(result)
+  const result2 = await client.features.isFeatureAvailableForAccount('test', '123')
+  console.log(result2)
 
-	/*      ACCOUNTS       */
-	
-		// const result3 = await client.accounts.post({ key: 'features', plans: ['123'] })
-		// console.log(result3)
+  /*      ACCOUNTS       */
+  // const result3 = await client.accounts.post({ key: 'features', plans: ['123'] })
+  // console.log(result3)
 
+  /*     METRICS        */
+  // const result4 = await client.metrics.setMetricsKey({ key: 'video-watched', value: '60', account_key: '123' })
+  // console.log(result4)
 
+  // const isBoolean = client.evaluation.getBooleanValue('key', true)
+  // console.log(isBoolean)
 
-	// /*     METRICS        */
-	// const result4 = await client.metrics.setMetricsKey({ key: 'features', value: '123', timestamp: new Date() })
-	// console.log(result4)
-
-	const isBoolean = client.evaluation.getBooleanValue('key', true)
-	console.log(isBoolean)
-
-	client.evaluation.getObjectValue('key', 'string')
+  // client.evaluation.getObjectValue('key', 'string')
 }
 
 exampleTests()
